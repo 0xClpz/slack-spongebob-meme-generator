5793cc6b112c5a99051bc456f355a56870cd8e2b
Antonio <antonio.root@gmail.com>
2019-11-14 00:10:52 +0100

    Adds build script
    Test
    
    Fix build script
    
    Fix build script
    
    Fix build script
    
    Fix build script
    
    Allows heroku to pass process.env.port
    
    adds ping endpoint
    
    Dirty test
    
    Fix build script
    
    Fix build script
    
    Fixes build scripts
    
    Ditches microbundle for tsc

--- memer-in-chief/src/index.ts
+++ memer-in-chief/src/index.ts
@@ -1,4 +1,4 @@
-import './env'
+import "./env";
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
@@ -7,25 +7,33 @@ import { generateSpongebobMeme } from "./helpers/memeHelpers";
 
 const app = express();
 
-app.post("/slack/generate", bodyParser.urlencoded({extended: false}), async (req, res) => {
-  res.sendStatus(200);
-  const { text, channel_id } = req.body;
-  const {stream, text: processedText} = await generateSpongebobMeme(text);
+app.post(
+  "/slack/generate",
+  bodyParser.urlencoded({ extended: false }),
+  async (req, res) => {
+    res.sendStatus(200);
+    const { text, channel_id } = req.body;
+    const { stream, text: processedText } = await generateSpongebobMeme(text);
 
-  const form = new FormData()
+    const form = new FormData();
 
-  form.append("token", process.env.SLACK_TOKEN)
-  form.append("title", processedText)
-  form.append("filename", "image.png")
-  form.append("filetype", "auto")
-  form.append("channels", channel_id)
-  form.append("file", stream, "image.png")
+    form.append("token", process.env.SLACK_TOKEN);
+    form.append("title", processedText);
+    form.append("filename", "image.png");
+    form.append("filetype", "auto");
+    form.append("channels", channel_id);
+    form.append("file", stream, "image.png");
 
-  await axios.post('https://slack.com/api/files.upload', form, {
-    headers: form.getHeaders()
-  })
-});
+    await axios.post("https://slack.com/api/files.upload", form, {
+      headers: form.getHeaders()
+    });
+  }
+);
 
-app.listen(9000, () => {
-  console.log(`🧽  Server up and running at port 9000 🧽`);
+app.get("/ping", (req, res) => res.send("pong"));
+
+app.listen(process.env.port || 9000, () => {
+  console.log(
+    `🧽  Server up and running at port ${process.env.port || 9000} 🧽`
+  );
 });
