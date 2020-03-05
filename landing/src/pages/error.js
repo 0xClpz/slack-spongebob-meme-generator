cc3b0aa840145229339355a6eca9e76ecaf24f6d
Antonio <antonio.root@gmail.com>
2020-03-05 22:29:34 +0100

    Adds landing page

---
+++ landing/src/pages/error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <h1>🤯</h1>
+}
+
+export default Error
---
+++ landing/src/pages/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <a href="https://slack.com/oauth/authorize?scope=commands,bot&client_id=977747011076.980065613159">
+      <img
+        height="40"
+        width="139"
+        src="https://platform.slack-edge.com/img/add_to_slack.png"
+        srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+      />
+    </a>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
